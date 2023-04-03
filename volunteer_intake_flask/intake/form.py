b72dbf30a97f4ef53a5df007eb37fc924b945bb8
mireles <mireles@horseless.io>
2023-04-03 14:31:24 -0400

    Add simple DB insertion code

--- volunteer_intake_flask/intake/form.py
+++ volunteer_intake_flask/intake/form.py
@@ -6,8 +6,9 @@ bp = Blueprint('form', __name__, url_prefix='/volunteer')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    print("Register")
     if request.method == 'POST':
-        name = request.form["name"]
+        name = request.form["username"]
         address = request.form["address"]
         city = request.form["city"]
         state = request.form["state"]
@@ -18,14 +19,54 @@ def register():
         cell_phone = request.form["cell_phone"]
         email = request.form["email"]
         dob = request.form["dob"]
+        active = 1
+
+        print("Got here")
+        error = None
+
+        db = get_db()
 
         # A lot of data cleaning will probably happen here.
         if not name:
             error = "Name is required."
-        
+
+        # Unsure the extent to which these will be required by Edwins
+        if not address:
+            address = "NA"
+        if not city:
+            city = "NA"
+        if not state:
+            state = "NA"
+        if not zip:
+            zip = "NA"
+        if not home_phone:
+            home_phone = "NA"
+        if not occupation:
+            occupation = "NA"
+        if not employer:
+            employer = "NA"
+        if not cell_phone:
+            cell_phone = "NA"
+        if not email:
+            email = "NA"
+        if not dob:
+            dob = "NA"
+
         if error is None:
             # TODO: build out the full intake form to match the schema.
-            pass
+            print("Error is None")
+            try:
+                db.execute(
+                    "INSERT INTO volunteer_small (active, name, address, city, state, zip, home_phone, occupation, employer, cell_phone, email, dob)" \
+                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (active, name, address, city, state, zip, home_phone, occupation, employer, cell_phone, email, dob),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {name} is already registered"
+            else:
+                print("Error is not None")
+                return redirect(url_for('hello'))
 
         flash(error)
     
