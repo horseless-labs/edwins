448c58426ce2bed303e8b940f5a0c2df46f2b15e
mireles <mireles@horseless.io>
2023-04-02 22:24:58 -0400

    Light expansion of registration code

--- volunteer_intake_flask/intake/form.py
+++ volunteer_intake_flask/intake/form.py
@@ -7,4 +7,22 @@ bp = Blueprint('form', __name__, url_prefix='/volunteer')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        pass
+        name = request.form["name"]
+        address = request.form["address"]
+        city = request.form["city"]
+        state = request.form["state"]
+        zip = request.form["zip"]
+        home_phone = request.form["home_phone"]
+        occupation = request.form["occupation"]
+        employer = request.form["employer"]
+        cell_phone = request.form["cell_phone"]
+        email = request.form["email"]
+        dob = request.form["dob"]
+
+        # A lot of data cleaning will probably happen here.
+        if not name:
+            error = "Name is required."
+        
+        if error is None:
+            # TODO: build out the full intake form to match the schema.
+            pass
