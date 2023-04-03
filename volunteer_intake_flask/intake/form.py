ab71ffcbbb745025eccc5877123eee8e0c68cb59
mireles <mireles@horseless.io>
2023-04-02 22:29:01 -0400

    Add CSS style sheet

--- volunteer_intake_flask/intake/form.py
+++ volunteer_intake_flask/intake/form.py
@@ -1,5 +1,5 @@
 import functools
-from flask import Blueprint, flash, g, redirect, render_template, request, session url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from intake.db import get_db
 
 bp = Blueprint('form', __name__, url_prefix='/volunteer')
