d03ef627a09f6307501ff923422b8bd19c4a88f8
mireles <mireles@horseless.io>
2023-04-04 01:00:49 -0400

    Expand opening functionality

--- volunteer_intake_word/src/load.py
+++ volunteer_intake_word/src/load.py
@@ -1,12 +1,29 @@
 from docx.api import Document
 
 src_file = "data/test.docx"
-document = Document(src_file)
 
-form = ""
-for table in document.tables:
-    #print("NEW TABLE")
-    for row in table.rows:
-        #print("|".join([cell.text for cell in row.cells]))
-        form += "|".join([cell.text for cell in row.cells]) + '\n'
-print(form)
+def open_document(src_file):
+    try:
+        document = Document(src_file)
+    except FileNotFoundError:
+        print(f"The file '{src_file}' could not be found")
+    except Exception as e:
+        print(f"An error occurred while trying to open {src_file}: {e}")
+    else:
+        print(f"Opening {src_file}")
+        return document
+
+def parse(document):
+    form = ""
+    for table in document.tables:
+        #print("NEW TABLE")
+        for row in table.rows:
+            #print("|".join([cell.text for cell in row.cells]))
+            form += "|".join([cell.text for cell in row.cells]) + '\n'
+    return form
+
+if __name__ == '__main__':
+    doc = open_document(src_file)
+    
+    if doc:
+        form = parse(doc)
