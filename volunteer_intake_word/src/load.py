690ab7ac99c9477503d2c6199044439ad62eb881
mireles <mireles@horseless.io>
2023-04-04 01:16:56 -0400

    Add default NA values to extract_fields()

--- volunteer_intake_word/src/load.py
+++ volunteer_intake_word/src/load.py
@@ -4,25 +4,49 @@ src_file = "data/test.docx"
 
 def open_document(src_file):
     try:
-        document = Document(src_file)
+        doc = Document(src_file)
     except FileNotFoundError:
         print(f"The file '{src_file}' could not be found")
     except Exception as e:
         print(f"An error occurred while trying to open {src_file}: {e}")
     else:
         print(f"Opening {src_file}")
-        return document
+        return doc
 
-def parse(document):
+def extract_fields(document):
     form = ""
     for table in document.tables:
         #print("NEW TABLE")
         for row in table.rows:
             #print("|".join([cell.text for cell in row.cells]))
-            form += "|".join([cell.text for cell in row.cells]) + '\n'
+            ueg = "|".join([cell.text for cell in row.cells])
+            if ueg.endswith("|"):
+                ueg += "NA\n"
+            else:
+                ueg += '\n'
+            form += ueg
     return form
+
+"""
+Noticing now that the extracted fields so far do not include check boxes.
+TODO:
+    - Add ability to extract check boxes to `extract_fields()`
+    - Add a dictionary
+"""
+def parse_fields(field_str):
+    rows = field_str.split('\n')
+    print(rows)
+
+    text_areas = []
+    for row in row:
+        if "|" not in row:
+            text_areas.append(row)
+            continue
 
 if __name__ == '__main__':
     doc = open_document(src_file)
     
-    form = parse(doc)
+    if doc:
+        form = extract_fields(doc)
+        #parse_fields(form)
+        print(form)
