c20df682438f0581a82ba1c6c9e4e72258836fcd
mireles <mireles@horseless.io>
2023-04-07 16:05:23 -0400

    Fixed list insertion problem

--- volunteer_intake_flask/intake/form.py
+++ volunteer_intake_flask/intake/form.py
@@ -4,6 +4,10 @@ from intake.db import get_db
 
 bp = Blueprint('form', __name__, url_prefix='/volunteer')
 
+def stringify_checkbox(checkbox):
+    str_checkbox = ', '.join(checkbox)
+    return str_checkbox
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -11,32 +15,52 @@ def register():
         name = request.form["username"]
         address = request.form["address"] or "NA"
         city = request.form["city"] or "NA"
-        state = request.form["state"] or "NA"
+        state = request.form["state"] or "--select a state--"
         zip = request.form["zip"] or "NA"
         home_phone = request.form["home_phone"] or "NA"
         occupation = request.form["occupation"] or "NA"
         employer = request.form["employer"] or "NA"
         cell_phone = request.form["cell_phone"] or "NA"
-        email = request.form["email"] or "NA"
+        email = request.form["email"] or "na@na.com"
         dob = request.form["dob"] or "NA"
+        print(active)
+        print(name)
+        print(address)
+        print(city)
+        print(state)
+        print(zip)
+        print(home_phone)
+        print(occupation)
+        print(employer)
+        print(cell_phone)
+        print(email)
+        print(dob)
 
-        times = request.form.getlist("times[]") or "NA"
+        times = request.form.getlist("times[]") or ["NA"]
+        times = stringify_checkbox(times)
 
-        selected_interests = request.form.getlist("interests[]") or "NA"
+        selected_interests = request.form.getlist("interests[]") or ["NA"]
         other_interests = request.form.get("other_interests") or "NA"
+        selected_interests = stringify_checkbox(selected_interests)
 
         skills = request.form.get("skills") or "NA"
         experience = request.form.get("experinece") or "NA"
 
-        oef = request.form.getlist("oef[]") or "NA"
-        students_lives = request.form.getlist("students_lives[]") or "NA"
+        oef = request.form.getlist("oef[]") or ["NA"]
+        oef = stringify_checkbox(oef)
+
+        students_lives = request.form.getlist("students_lives[]") or ["NA"]
         other_students_lives = request.form.get("other_students_lives") or "NA"
+        students_lives = stringify_checkbox(students_lives)
 
-        class_education = request.form.getlist("class_education[]") or "NA"
+        class_education = request.form.getlist("class_education[]") or ["NA"]
         guest_speaker = request.form.get("guest_speaker") or "NA"
+        class_education = stringify_checkbox(class_education)
 
-        facilities = request.form.getlist("facilities[]") or "NA"
-        clerical_advo = request.form.getlist("clerical_advo[]") or "NA"
+        facilities = request.form.getlist("facilities[]") or ["NA"]
+        clerical_advo = request.form.getlist("clerical_advo[]") or ["NA"]
+        facilities = stringify_checkbox(facilities)
+        clerical_advo = stringify_checkbox(clerical_advo)
 
         error = None
 
@@ -46,44 +70,25 @@ def register():
         if not name:
             error = "Name is required."
 
-        # Unsure the extent to which these will be required by Edwins
-        """
-        if not address:
-            address = "NA"
-        if not city:
-            city = "NA"
-        if state == "--select a state--":
-            state = "NA"
-        if not zip:
-            zip = "NA"
-        if not home_phone:
-            home_phone = "NA"
-        if not occupation:
-            occupation = "NA"
-        if not employer:
-            employer = "NA"
-        if not cell_phone:
-            cell_phone = "NA"
-        if not email:
-            email = "NA"
-        if not dob:
-            dob = "NA"
-        """
-
         if error is None:
             # TODO: build out the full intake form to match the schema.
             print("Error is None")
             try:
                 db.execute(
-                    "INSERT INTO volunteer (active, name, address, city, state, zip, home_phone, occupation, employer, cell_phone, email, dob," \
-                    " times, selected_interests, other_interests, skills, experience, oef, students_lives, other_students_lives, class_education," \
-                    " guest_speaker, facilities, clerical_advo)" \
-                    " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    "INSERT INTO volunteer (active, name, address, city, state, zip, home_phone, occupation, employer, cell_phone, email, dob, \
+                    times, selected_interests, other_interests, skills, experience, oef, students_lives, other_students_lives, class_education, \
+                    guest_speaker, facilities, clerical_advo) \
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                     (active, name, address, city, state, zip, home_phone, occupation, employer, cell_phone, email, dob, times, selected_interests,
                      other_interests, skills, experience, oef, students_lives, other_students_lives, class_education, guest_speaker, facilities, clerical_advo),
                 )
                 #print(selected_interests)
                 #print(other_interests)
+                print(oef)
+                print(students_lives)
+                print(class_education)
+                print(facilities)
+                print(clerical_advo)
                 #db.commit()
             except db.IntegrityError:
                 error = f"User {name} is already registered"
--- volunteer_intake_word/src/load.py
+++ volunteer_intake_word/src/load.py
@@ -59,6 +59,19 @@ def parse_check_boxes(paragraphs):
             boxes.append(para)
     return boxes
 
+# Reduces a list of check boxes to a small number of strings, one for each of the
+# broader categories that they are found in on the volunteer intake form.
+def reduce_boxes(boxes):
+    selected_interests = boxes[:7]
+    # Things like other_interests are not collected from checkboxes.
+
+    oef = boxes[7:12]
+    students_lives = boxes[12:19]
+    class_education = boxes[19:23]
+    facilities = boxes[23:32]
+    clerical_advo = boxes[32:40]
+    print(clerical_advo)
+
 if __name__ == '__main__':
     doc = open_document(src_file)
 
@@ -70,5 +83,10 @@ if __name__ == '__main__':
         boxes = parse_check_boxes(paragraphs)
         form = extract_fields(doc)
         #parse_fields(form)
-        print(form)
-        print(boxes)
+        #print(form)
+        #print(boxes)
+        """
+        for i in range(len(boxes)):
+            print(f"{i}: {boxes[i]}")
+        """
+        reduce_boxes(boxes)
